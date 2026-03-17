249c2333272c116c330a98529cd7ab5298749efe
Jesus Serrano Alarcos <jesus.serrano@cojali.com>
2026-03-17 08:26:40 +0100

    Noveno capítulo del curso de REACT desde 0

---
+++ gif-app/src/App.js
@@ -0,0 +1,41 @@
+import { Link, Route } from "wouter";
+import Detail from 'pages/Detail';
+import React from 'react';
+import SearchResults from 'pages/SearchResults';
+import StaticContext from 'context/StaticContext';
+import {GifsContextProvider} from 'context/GifsContext';
+import './App.css';
+import ErrorPage from "pages/ErrorPage";
+import Header from "components/Header";
+import Login from "pages/Login";
+import {UserContextProvider} from "context/UserContext";
+
+const HomePage = React.lazy(() => import("pages/Home"));
+
+function App() {
+  return (
+    <UserContextProvider>
+    <div className="App">
+      <section className='App-content'>
+        <h1>Capítulo 9 - REACT desde 0</h1>
+        <Link to='/' className={"PaginaPrincipal"}>Página principal</Link>
+        <GifsContextProvider>
+                <Header />
+                <Route component={HomePage} path="/" />
+                <Route
+                  component={SearchResults}
+                  path="/search/:keyword/:rating?"
+                />
+                <Route component={Detail} path="/gif/:id" />
+                <Route component={Login} path="/login" />
+                <Route
+                component={ErrorPage}
+                path="/404"/>
+        </GifsContextProvider>
+      </section>
+    </div>
+    </UserContextProvider>
+  );
+}
+
+export default App;
---
+++ gif-app/src/hooks/useUser.js
@@ -0,0 +1,44 @@
+import { useCallback, useContext, useState } from "react";
+import Context from "context/UserContext";
+import loginService from "services/login";
+import { UserContextProvider } from "context/UserContext";
+import addFavService from "services/addFav"
+
+export default function useUser () {
+    const {favs, jwt, setFavs, setJWT} = useContext(Context)
+    const [state, setState] = useState({loading: false, error: true})
+
+    const login = useCallback(({username, password}) => {
+        setState({loading: true, error: false})
+        loginService({username, password})
+            .then(jwt => {
+                setState({loading: false, error: false})
+                setJWT(jwt)
+            })
+            .catch(err => {
+                setState({loading: false, error: true})
+                console.error(err)
+            })
+    }, [setJWT])
+
+    const fav = useCallback(({id}) => {
+        addFavService({id, jwt})
+            .then(favs => setFavs(favs))
+            .catch(err => {
+                console.error(err)
+            })  
+    }, [jwt, setFavs])
+
+    const logout = useCallback(() => {
+        window.sessionStorage.removeItem('jwt')
+        setJWT(null)
+    }, [setJWT])
+
+    return {
+        isLogged: Boolean(jwt),
+        isLoginLoading: state.loading,
+        hasLoginError: state.error,
+        login,
+        logout
+    }
+}
